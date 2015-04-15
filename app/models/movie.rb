97dd63c40cf911349318f61170aec17ed69600fb
katchy <priya.thanya@gmail.com>
2015-04-15 01:24:39 +0000

    resolved merge conflicts in model/movies/rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Movie < ActiveRecord::Base
   has_many :reviews
   
@@ -30,6 +29,13 @@ class Movie < ActiveRecord::Base
       end
 
 
+
+      def self.search(query)
+        # where(:title, query) -> This would return an exact match of the query
+        where("title like ?", "%#{query}%") 
+      end
+
+
       protected
 
       def release_date_is_in_the_future
@@ -39,52 +45,3 @@ class Movie < ActiveRecord::Base
       end
 
 end
-=======
-class Movie < ActiveRecord::Base
-  has_many :reviews
-  
-  mount_uploader :image, ImageUploader
-
-  validates :title,
-  presence: true
-
-  validates :director,
-  presence: true
-
-  validates :runtime_in_minutes,
-  numericality: { only_integer: true }
-
-  validates :image,
-  presence: true
-
-  validates :description,
-  presence: true
-
-  validates :release_date,
-  presence: true
-
-  validate :release_date_is_in_the_future
-
-  def review_average
-    if reviews.size == 0
-      return 1
-    else
-      reviews.sum(:rating_out_of_ten)/reviews.size
-    end
-  end
-
-  def self.search(query)
-    # where(:title, query) -> This would return an exact match of the query
-    where("title like ?", "%#{query}%") 
-  end
-
-  protected
-
-  def release_date_is_in_the_future
-    if release_date.present?
-      errors.add(:release_date, "should probably be in the future") if release_date < Date.today
-    end
-  end
-
-end
->>>>>>> feature/admins
